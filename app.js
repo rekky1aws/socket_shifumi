66d1104a144d139f258a8b6af6a96f6ed0c70445
rekky1aws <killikomfr@gmail.com>
2026-09-23 05:30:15 +0200

    Starting frontend code
    Index page with all the rooms
    Create page to create a new room

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+// Socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const port = 3000;
+
+const rooms = {
+
+};
+
+const users = {
+  online: {},
+  offline: {}
+};
+
+app.use(express.static('public'));
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/create', (req, res) => {
+  res.sendFile(__dirname + '/public/create.html');
+});
+
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.emit('updateUsers', users); // Emit to only the person who just connected
+
+  // Listening to events emitted by clients
+  socket.on('newUser', (username) => {
+    // Adding user
+    users.online[socket.id] = {
+      name: username
+    }
+
+    // Send new user to everyone
+    io.emit('updateUsers', users);
+  });
+
+  socket.on('disconnect', (reason) => {
+    // console.log(`An user has disconnected (${users.online[socket.id].name}) for this reason : ${reason}`); // DEBUG
+    
+    users.offline[socket.id] = users.online[socket.id];
+    delete users.online[socket.id];
+    
+    io.emit('updateUsers', users);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app running on port : ${port}`); // DEBUG
+});
---
+++ public/javascripts/index.js
@@ -0,0 +1,18 @@
+// CONSTANTS
+const socket = io();
+
+    // Elements
+const newRoomBtn = document.querySelector('new-rom-btn');
+
+// VARIABLES
+let lclRooms = {};
+
+// FUNCTIONS
+function createRoom () {
+
+}
+
+// EVENT LISTENERS
+newRoomBtn.addEventListener('click', createRoom);
+
+// MAIN
